a5748fcc86dfe4bf2e1a965dd7e20c80e7ab2671
yigewu <yigewu@wustl.edu>
2023-10-02 14:36:56 -0500

    first commit after ding lab

---
+++ testing/docker_call/Individual.20200207.v1/run_sample.CPT0086350004.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+snRNA_aliquot_id=CPT0086350004
+analysis_mode=subclusters
+dir_run=/diskmnt/Projects/ccRCC_scratch/
+docker_image_name=singlecellportal/infercnv
+dir_infercnv=${dir_run}Resources/snRNA_Processed_Data/InferCNV/
+dir_infercnv_outputs=${dir_infercnv}outputs/
+dir_infercnv_inputs=${dir_infercnv}inputs/
+run_id=20200207.v1
+dir_infercnv_outputs_by_run=${dir_infercnv_outputs}Individual.${run_id}/
+mkdir -p ${dir_infercnv_outputs_by_run}
+dir_output=${dir_infercnv_outputs_by_run}${snRNA_aliquot_id}/
+mkdir -p ${dir_output}
+dir_raw_count_file=${dir_infercnv_inputs}raw_counts_matrix/
+path_raw_count_file=${dir_raw_count_file}${snRNA_aliquot_id}.RNA_Count.tsv
+dir_annotation_file=${dir_infercnv_inputs}annotations_file/Individual.${run_id}/
+path_annotation_file=${dir_annotation_file}${snRNA_aliquot_id}.Barcode_Annotation.txt
+path_log_file=${dir_output}${snRNA_aliquot_id}.$(date +%Y%m%d%H%M%S).log
+path_gene_order_file=/diskmnt/Projects/ccRCC_scratch/Resources/snRNA_Processed_Data/InferCNV/inputs/gencode_v21_gen_pos.MatchCellRangerFeatures.NoDuplicates.20191005.v1.txt
+cutoff=0.04
+ref_group_names=0,3,4,7,8,9,10,11
+docker run -v ${dir_run}:${dir_run} ${docker_image_name} inferCNV.R \
+        --analysis_mode=${analysis_mode} \
+        --raw_counts_matrix=${path_raw_count_file} \
+        --annotations_file=${path_annotation_file} \
+        --gene_order_file=${path_gene_order_file} \
+        --cutoff=${cutoff} \
+        --out_dir=${dir_output} \
+        --cluster_by_groups \
+        --denoise \
+        --HMM \
+        --num_threads=20 \
+        --ref_group_names=${ref_group_names} &> ${path_log_file}&
+
+
+
+
+
---
+++ testing/docker_call/run.20210805/run_sample.CPT0001270002.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+snRNA_aliquot_id=CPT0001270002
+analysis_mode=subclusters
+dir_run=/diskmnt/Projects/ccRCC_scratch/ccRCC_snRNA/
+docker_image_name=singlecellportal/infercnv
+dir_infercnv=${dir_run}Resources/snRNA_Processed_Data/InferCNV/
+dir_infercnv_outputs=${dir_infercnv}outputs/
+dir_infercnv_inputs=${dir_infercnv}inputs/
+run_id=run.20210805
+dir_infercnv_outputs_by_run=${dir_infercnv_outputs}${run_id}/
+mkdir -p ${dir_infercnv_outputs_by_run}
+dir_output=${dir_infercnv_outputs_by_run}${snRNA_aliquot_id}/
+mkdir -p ${dir_output}
+dir_raw_count_file=${dir_infercnv_inputs}raw_counts_matrix/doublets_removed/
+path_raw_count_file=${dir_raw_count_file}${snRNA_aliquot_id}.RNA_Count.tsv
+dir_annotation_file=${dir_infercnv_inputs}annotations_file/${run_id}/
+path_annotation_file=${dir_annotation_file}${snRNA_aliquot_id}.Barcode_Annotation.txt
+path_log_file=${dir_output}${snRNA_aliquot_id}.$(date +%Y%m%d%H%M%S).log
+path_gene_order_file=${dir_infercnv_inputs}gencode_v21_gen_pos.MatchCellRangerFeatures.NoDuplicates.20191005.v1.txt
+cutoff=0.04
+ref_group_names=Ref
+docker run -v ${dir_run}:${dir_run} --ulimit stack=8277716992:8277716992 ${docker_image_name} inferCNV.R \
+        --analysis_mode=${analysis_mode} \
+        --raw_counts_matrix=${path_raw_count_file} \
+        --annotations_file=${path_annotation_file} \
+        --gene_order_file=${path_gene_order_file} \
+        --cutoff=${cutoff} \
+        --out_dir=${dir_output} \
+        --cluster_by_groups \
+        --denoise \
+        --HMM \
+        --num_threads=20 \
+        --ref_group_names=${ref_group_names} &> ${path_log_file}&
+
+
+
+
+
